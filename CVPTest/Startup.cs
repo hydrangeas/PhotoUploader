5ed45f428e485db7d89a2281b913dff933e36ad5
hyd <hydrangeas.project@gmail.com>
2019-08-10 18:44:20 +0900

    MVCを使用するよう変更

--- CVPTest/Startup.cs
+++ CVPTest/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CVPTest
@@ -15,6 +16,7 @@ namespace CVPTest
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,8 +26,12 @@ namespace CVPTest
             {
                 app.UseDeveloperExceptionPage();
             }
-            var options = new DefaultFilesOptions();
-            app.UseDefaultFiles(options);
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Index}/{action=Index}/{id?}");
+            });
 
             //app.Run(async (context) =>
             //{
