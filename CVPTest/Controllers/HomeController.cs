520ea623305830d59e50c9368431a49d510b890c
hyd <hydrangeas.project@gmail.com>
2019-08-14 00:54:55 +0900

    ページネーションを意識し、1ページ20アイテム分だけ取得

--- CVPTest/Controllers/HomeController.cs
+++ CVPTest/Controllers/HomeController.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using CVPTest.ViewModels;
+using CVPTest.Models;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -11,10 +9,32 @@ namespace CVPTest.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly JobDatabase jobDatabase;
+
+        public HomeController(JobDatabase _jobDatabase)
+        {
+            jobDatabase = _jobDatabase;
+        }
+
         [HttpGet]
         public IActionResult Index()
         {
-            return View();
+            var model = GetJobList(1);
+            var viewModel = new IndexViewModel(model);
+            return View(viewModel);
+        }
+
+        public List<Job> GetJobList(int page)
+        {
+            var pageSize = 20;
+            var pageNumber = page - 1;
+
+            var jobs = jobDatabase.Jobs
+                                  .OrderByDescending(j => j.Modified)
+                                  .Skip(pageSize * pageNumber)
+                                  .Take(pageSize)
+                                  .ToList();
+            return jobs;
         }
     }
 }
