46115779626abe7e662a15b5b694603dd39c2f6b
hyd <hydrangeas.project@gmail.com>
2019-08-14 00:56:51 +0900

    アイテム表示用のVIewを追加

---
+++ CVPTest/Common/U.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace CVPTest.Common
+{
+    public class U
+    {
+        public class PartialViews
+        {
+            public const string ListOfJobs = "_pv_ListOfJobs";
+        }
+    }
+}
