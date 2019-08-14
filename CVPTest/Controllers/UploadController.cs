70227a72aeb42f58b24ff92d257134af3c308c44
hyd <hydrangeas.project@gmail.com>
2019-08-14 11:16:09 +0900

    Upload用のページは分離（まずはページだけ）

---
+++ CVPTest/Controllers/UploadController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CVPTest.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CVPTest.Controllers
+{
+    public class UploadController : Controller
+    {
+        // GET: /Upload/
+        public IActionResult Index()
+        {
+            var pageConfigure = new PageConfigure()
+            {
+                IsUpload = true
+            };
+            ViewData["PageConfigure"] = pageConfigure;
+
+            return View();
+        }
+    }
+}
